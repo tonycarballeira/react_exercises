c18b12e41857d06d4517cc69d26278055991da6a
tonycarballeira <tonycarballeira@gmail.com>
2023-02-16 13:29:20 -0500

    modified component

--- src/components/StateDropdown.tsx
+++ src/components/StateDropdown.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import "./StateDropdown.css";
 import { states } from "./states"
 
 export function StateDropdown() {
 
     const [isDropdownDisplayed, setIsDropdownDisplayed] = useState(false);
-
-
+    const [selectedStates, setSelectedStates] = useState(
+        states.reduce((obj, state) => ({...obj, [state.abbreviation]: false}))    
+    );
+        console.log("selected states", selectedStates);
     return (
         <fieldset className="state-dropdown">
             <button onClick={() => setIsDropdownDisplayed(prevState => !prevState)}> 
@@ -16,9 +18,16 @@ export function StateDropdown() {
             {isDropdownDisplayed && (
                 <div className="panel">
                     {states.map((state) => (
-                        <fieldset>
-                            <input id={state.abbreviation} type="checkbox" />
-                            <label htmlFor={state.abbreviation}>{state.name}</label>
+                        <fieldset key={state.abbreviation}>
+                            <input 
+                                
+                                
+                                id={state.abbreviation} 
+                                type="checkbox" 
+                            />
+                            <label htmlFor={state.abbreviation}>
+                                {state.name}
+                            </label>
                         </fieldset>
                     ))}
                 </div>
