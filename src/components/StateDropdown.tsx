3532983d2cbc8f61cd1e53d0c45b2471fc56b457
tonycarballeira <tonycarballeira@gmail.com>
2023-02-16 12:58:28 -0500

    added styling to dropdown menu

--- src/components/StateDropdown.tsx
+++ src/components/StateDropdown.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import "./StateDropdown.css"
+import "./StateDropdown.css";
+import { states } from "./states"
 
 export function StateDropdown() {
 
@@ -8,12 +9,20 @@ export function StateDropdown() {
 
     return (
         <fieldset className="state-dropdown">
-            <button 
-            onClick={() => setIsDropdownDisplayed(prevState => !prevState)}
-            > 
-            -- Select your states --
+            <button onClick={() => setIsDropdownDisplayed(prevState => !prevState)}> 
+            -- select your states --
             </button>
-            {isDropdownDisplayed && <div className="panel">Hello</div>}
+            
+            {isDropdownDisplayed && (
+                <div className="panel">
+                    {states.map((state) => (
+                        <fieldset>
+                            <input id={state.abbreviation} type="checkbox" />
+                            <label htmlFor={state.abbreviation}>{state.name}</label>
+                        </fieldset>
+                    ))}
+                </div>
+            )}
         </fieldset>
     )
 }
